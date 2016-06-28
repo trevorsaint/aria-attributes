321a4316dc90a6070e016a685e9212a5f0be21d4
Trevor Saint <trevor@infigomedia.uk>
2016-06-28 14:23:50 +0100

    Added automatic tasks in grunt. Added sass cache ignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,36 +25,42 @@ module.exports = function (grunt) {
         }
 
       },
+      
+      
+      sass: {
+
+        dist: {
+
+          options: {
+            noCache: true,
+            style: 'expanded',
+            sourcemap: 'none'
+          },
+          
+          files: {
+            'public/styles/main.css' : 'public/sass/main.scss', 
+          }
+
+        }
+
+      },
 
 
       watch: {
 
         options: {
           livereload: true,
+          spawn: false
         },
   
         express: {
-
           files: ['app.js', 'Gruntfile.js', 'routes/*.js'],
-          tasks: ['express:dev'],
-
-          options: {
-            livereload: true,
-            spawn: false
-          }
-
+          tasks: ['express:dev']
         },
         
-        jade: {
-
-          files: ['views/**/*.jade'],
-          tasks: ['jade'],
-
-          options: {
-            livereload: true,
-            spawn: false
-          }
-
+        sass: {
+          files: ['public/sass/**/*.scss'],
+          tasks: ['sass']
         }
         
       }
@@ -63,14 +69,12 @@ module.exports = function (grunt) {
   });
 
 
-  ['grunt-express-server',
-   'grunt-contrib-watch'
-  ].forEach(function (task) {
+  ['grunt-express-server', 'grunt-contrib-sass', 'grunt-contrib-watch'].forEach(function (task) {
     grunt.loadNpmTasks(task);
   });
 
 
-  grunt.registerTask('default', ['express:dev', 'watch']);
+  grunt.registerTask('default', ['express:dev', 'sass', 'watch']);
 
 
 };
